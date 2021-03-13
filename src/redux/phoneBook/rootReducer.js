7ce8e14b32361ca2cd99baf1e0143aab3d406d91
Pavel Sakun <axelhomkin@gmail.com>
2021-03-13 21:37:37 +0200

    some changes

--- src/redux/phoneBook/rootReducer.js
+++ src/redux/phoneBook/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import phoneBookReducer from './phoneBook.reduser';
+import phoneBookReducer from './phoneBook.reducer';
 
 const rootReducer = combineReducers({
   contacts: phoneBookReducer,
